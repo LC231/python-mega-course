0147d93fb3404747823657c659c572902bf4d320
231lukecurran@gmail.com <231lukecurran@gmail.com>
2025-07-02 20:41:15 +0100

    complete and exit

--- 1st Project/GUI.py
+++ 1st Project/GUI.py
@@ -8,9 +8,14 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=Functions.get_todos(), key='todos', 
                       enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-do App", 
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]], 
+                   layout=[[label], 
+                           [input_box, add_button], 
+                           [list_box, edit_button, complete_button],
+                           [exit_button]], 
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
@@ -23,7 +28,7 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             Functions.write_todos(todos)
-            window['todos'].Update(values=todos)
+            window['todos'].update(values=todos)
             
         case "Edit":
             todo_to_edit = values['todos'][0]
@@ -33,10 +38,21 @@ while True:
             index = todos.index(todo_to_edit)
             todos[index] = new_todo
             Functions.write_todos(todos)
-            window['todos'].Update(values=todos)
+            window['todos'].update(values=todos)
+        
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = Functions.get_todos()
+            todos.remove(todo_to_complete)
+            Functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+
+        case "Exit":
+            break
         
         case'todos':
-            window['todo'].Update(value=values['todos'][0])
+            window['todo'].update(value=values['todos'][0])
         
         case sg.WIN_CLOSED:
             break
