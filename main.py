95bc5bd8393254cd1270b1ce821ad84687e2ce30
231lukecurran@gmail.com <231lukecurran@gmail.com>
2025-07-04 15:43:23 +0100

    organization

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ st.write(content2)
 
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
-df = pandas.read_csv("Second_project/data.csv",sep=";")
+df = pandas.read_csv("Second_Project/data.csv",sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
