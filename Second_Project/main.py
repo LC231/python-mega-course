1c84a4639cf4c26830da2c7badd4f3dabf44d96d
231lukecurran@gmail.com <231lukecurran@gmail.com>
2025-07-04 14:56:48 +0100

    description images and links

--- Second_Project/main.py
+++ Second_Project/main.py
@@ -17,18 +17,26 @@ I have worked with companies from various countries, such as the Center for Cons
     st.info(content)
 
 content2 = """
-Below you cna find some of the apps I have built in Python. Feel free to contact me
+Below you can find some of the apps I have built in Python. Feel free to contact me
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("Second_project/data.csv",sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("Second_Project/images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("Second_Project/images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
